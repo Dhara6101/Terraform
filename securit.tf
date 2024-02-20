1866b7f4bcabf1258eadd41854157fe88538f524
dharagit <dhara6101@gmail.com>
2024-02-20 17:38:17 +0530

    add sg with terraform

---
+++ securit.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Security group for HTTP and HTTPS traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere for HTTP
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere for HTTPS
+  }
+}
